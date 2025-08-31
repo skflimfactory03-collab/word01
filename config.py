c54328a0a71596a0d8c4d0a5a6abb7d91c2ab0fe
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-08-31 19:47:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()  # loads .env file automatically
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8447850903:AAFWZcZwT47xlvC8KuNDFCOmKCRj_F6F76U"),
+BOT_TOKEN = os.getenv('8447850903:AAFWZcZwT47xlvC8KuNDFCOmKCRj_F6F76U'),
