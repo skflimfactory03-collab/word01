78be0063e9f3df33b15238eb8405b4cdfd785f2d
skflimfactory03-collab <skflimfactory03@gmail.com>
2025-09-02 10:46:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()  # loads .env file automatically
 
-BOT_TOKEN = '7495023640:AAHA854K18m74imKGgkNITQm-Dgny3PEaNI'
+BOT_TOKEN = '7495023640:AAFJmmcAVBTsqLgWUiqn09pi0wYVcKfPnd4'
